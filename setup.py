f7badf8356a45b9081e9ed8d75cd732339aacfdd
Dun Liang <randonlang@gmail.com>
2022-11-30 13:13:17 +0800

    add  grad hook

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="jtorch",
-    version="0.0.5",
+    version="0.0.6",
     author="jtorch",
     author_email="jtorch@qq.com",
     description="jtorch project",
@@ -24,6 +24,6 @@ setuptools.setup(
     package_data={'': ['*', '*/*', '*/*/*','*/*/*/*','*/*/*/*/*','*/*/*/*/*/*']},
     python_requires=">=3.7",
     install_requires=[
-        "jittor>=1.3.4.10",
+        "jittor>=1.3.5.40",
     ],
 )
