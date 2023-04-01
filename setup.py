684cc01a4051fd97214c2159a98dd36fb4b145d4
Dun Liang <randonlang@gmail.com>
2023-04-02 07:25:44 +0800

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="jtorch",
-    version="0.1.0",
+    version="0.1.1",
     author="jtorch",
     author_email="jtorch@qq.com",
     description="jtorch project",
